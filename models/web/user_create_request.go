f9952ccd87c0340026b65eb9f5874fb229c9670e
ganggas <subhannizar25@gmail.com>
2023-12-28 11:34:18 +0800

    Setup roles as field in request payload and user responses

--- models/web/user_create_request.go
+++ models/web/user_create_request.go
@@ -1,8 +1,13 @@
 package web
 
+type UserRoleRequest struct {
+	RoleID uint `json:"role_id" validate:"required"`
+}
+
 type UserCreateRequest struct {
-	Name     string `json:"name" validate:"required"`
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8,max=32"`
-	Username string `json:"username" validate:"required,min=3,max=32"`
+	Name     string            `json:"name" validate:"required"`
+	Email    string            `json:"email" validate:"required,email"`
+	Password string            `json:"password" validate:"required,min=8,max=32"`
+	Username string            `json:"username" validate:"required,min=3,max=32"`
+	Roles    []UserRoleRequest `json:"roles" validate:"required"`
 }
--- models/web/user_response.go
+++ models/web/user_response.go
@@ -2,18 +2,32 @@ package web
 
 import "peken-be/models/domain"
 
+type UserRoleResponse struct {
+	RoleID   uint   `json:"id"`
+	RoleName string `json:"name"`
+}
+
 type UserResponse struct {
-	Id       uint   `json:"id"`
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Name     string `json:"name"`
+	Id       uint               `json:"id"`
+	Email    string             `json:"email"`
+	Username string             `json:"username"`
+	Name     string             `json:"name"`
+	Roles    []UserRoleResponse `json:"roles"`
 }
 
 func NewUserResponse(user domain.User) UserResponse {
+	var roles []UserRoleResponse
+	for _, role := range user.Roles {
+		roles = append(roles, UserRoleResponse{
+			RoleID:   role.ID,
+			RoleName: role.Name,
+		})
+	}
 	return UserResponse{
 		Id:       user.Id,
 		Email:    user.Email,
 		Username: user.Username,
 		Name:     user.Name,
+		Roles:    roles,
 	}
 }
